05d1715a4cca060d486bcb859ebc77eea225c845
Shawn Hassen <Shassen89@gmail.com>
2018-08-02 21:56:17 -0400

    Successfully fetched object from newsapi in api.js

--- api.js
+++ api.js
@@ -1,3 +1,21 @@
-
+const fetch = require('node-fetch');
+const axios = require('axios');
 const apiKey = process.env.NEWS_API_KEY;
 
+function test() {
+  return fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=eca1e2899a2b41f48bee1f59b9c33aa5')
+    .then((resp) => {
+      if (!resp.ok) {
+        throw Error(resp.statusText);
+      }
+      return resp.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((e) => {
+      console.log(e.message);
+    });
+}
+
+test();
